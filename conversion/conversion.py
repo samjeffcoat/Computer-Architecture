275a3d22e1ae9bb61e4207fe591ac53640f30cbf
Sam <Jeffcoat>
2019-10-14 17:06:04 -0500

    adds lecture notes

---
+++ conversion/conversion.py
@@ -0,0 +1,15 @@
+str = "101110"
+
+
+def to_decimal(num_string, base):
+    digit_list = list(num_string)
+    digit_list.reverse()
+    value = 0
+    for i in range(len(digit_list)):
+        print(f"+({int(digit_list[i])} * {base ** i})")
+        value += int(digit_list[i]) * (base ** i)
+    return value
+
+
+​
+to_decimal(str, 2)
